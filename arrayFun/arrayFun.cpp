ebb07d640f7fe8490ecbd8785684ee471bb4c198
Peter Moldenhauer <molde003@gmail.com>
2016-07-19 14:22:05 -0500

    Modified arrayFun file

--- arrayFun/arrayFun.cpp
+++ arrayFun/arrayFun.cpp
@@ -21,6 +21,8 @@ int main()
     for (int i=0; i<size; i++)  //print out all of the array elements using a loop
         cout << array[i] << " ";
 
+
+
     //calculate the sum of the array...
     int sum = 0;
     for (int i =0; i<size; i++)
@@ -28,13 +30,38 @@ int main()
 
     cout << "\n\nThis is the sum of the array elements: " << sum << endl;  //print out sum of array elements
 
+
+
     cout << "\nThese are the elements of the array (again):" << endl;
     printArray(array, size);  //print out the array elements by calling the function outside of main
     //Note: Above when the printArray function is called, just use the array name without [] to use the array as an argument to be passed
 
+
+    //Below I will find the highest value in the array...
+    int highest;
+    highest = array[0];
+    for(int i = 0; i < size; i++)
+    {
+        if (array[i] > highest)
+            highest = array[i];
+    }
+    cout << "\n\nThe highest value in the array is: " << highest << endl;
+
+
+    //Below I will find the lowest value in the array..
+    int lowest;
+    lowest = array[0];
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i] < lowest)
+            lowest = array[i];
+    }
+    cout << "\nThe lowest value in the array is: " << lowest << endl;
+
+
     //Below I will demonstrate the concept of multidimensional arrays
     int multiArray[2][3] = {{1,2,3},{7,8,9}};  //define a 2D array with 2 rows and 3 columns
-    cout << "\n\nThese are the elements in a 2-dimensional array: \n" << endl;
+    cout << "\nThese are the elements in a 2-dimensional array: \n" << endl;
     for(int row=0; row<2; row++)  //print out the contents of the 2D array using nested loop for rows and columns
     {
         for(int column=0; column<3; column++) //nested for loop for columns in each row iteration
