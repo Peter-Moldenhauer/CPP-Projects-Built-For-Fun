e245ec7aeb76cc533662867197bd158122d5ac5d
Peter Moldenhauer <molde003@gmail.com>
2016-08-19 17:16:52 -0500

    Added functions in separate files

---
+++ functionsSeparateFiles/function.cpp
@@ -0,0 +1,7 @@
+#include "function.h"
+#include <iostream> 
+using namespace std;
+
+void myFunction(){
+	cout << "This text is printed from the function" << endl;
+}
---
+++ functionsSeparateFiles/mainFunction.cpp
@@ -0,0 +1,9 @@
+#include "function.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	myFunction();
+
+	return 0;
+}
